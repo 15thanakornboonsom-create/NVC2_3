2ee7167f93a1bafc1325f3b207921ac3434dabbc
mai992 <sommai992za@gmail.com>
2025-11-03 10:58:11 +0700

    1

--- 96_atchasai/index.php
+++ 96_atchasai/index.php
@@ -8,7 +8,8 @@
 </head>
 <body>
 
-
-    
+<h1 class="">Hello Word</h1>
+<h2 class="" >Hello Everyone</h2>
+<p class="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis nesciunt odit praesentium nulla voluptatum eveniet, tempora modi id rem molestias fugit quae, incidunt iure dolor odio quos qui, soluta iste?</p>
 </body>
 </html>
