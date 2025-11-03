089a4331e78e192fde74854facae933e53b40644
Antagonist <thanadonpremmxc@gmail.com>
2025-11-03 10:57:35 +0700

    f

---
+++ 100-Phoemphun/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sixone</title>
+</head>
+
+
+
+<body>
+    
+<h1 class="">helloWworld</h1>
+<h2 class="">world heello</h2>
+
+</body>
+
+
+
+</html>
