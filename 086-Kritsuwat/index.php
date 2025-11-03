b9f79569ce4f7c3080f48800a472f94c57be1e39
jakjakgapat-byte <time.1430ll@gmail.com>
2025-11-03 10:45:14 +0700

    hello

--- 086-Kritsuwat/index.php
+++ 086-Kritsuwat/index.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ez</title>
 </head>
+
+
+
 <body>
-    
+
+<h1 class="">Hello world</h1>
+
 </body>
+
+
+
 </html>
--- 090-raksit/index.php
+++ 090-raksit/index.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+
+
+
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>wawa</title>
 </head>
+
+
+
 <body>
-    
+
+<h1 class="">Hello wa</h1>
+
 </body>
+
+
+
 </html>
