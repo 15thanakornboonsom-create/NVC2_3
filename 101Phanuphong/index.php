65c80b00d4f4d02ae2a9b975464ab769b14a1796
fourthrainy-sudo <ganjetawat@gmail.com>
2025-11-03 10:56:36 +0700

    w

--- 101Phanuphong/index.php
+++ 101Phanuphong/index.php
@@ -6,6 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    
+<h1 class="">sawade</h1>    
 </body>
 </html>
--- 89chawanakorn/index.php
+++ 89chawanakorn/index.php
@@ -7,7 +7,9 @@
 </head>
 
 <body>
-
+<h1 class="">sawad</h1>
+<h2 class="">wawa</h2>
+<p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos eveniet vero culpa dolores deserunt animi possimus sunt voluptatibus voluptate, excepturi amet cum vel iste assumenda numquam aliquid, odio eaque. Ipsam?</p>
 
 
 </body>
