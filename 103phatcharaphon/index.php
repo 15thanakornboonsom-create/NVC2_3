f9a5deffac8168705f6af7789f179f755ed9c3a9
kawzx123 <dawkrathok@gmail.com>
2025-11-10 09:51:26 +0700

    .

--- 103phatcharaphon/index.php
+++ 103phatcharaphon/index.php
@@ -12,6 +12,9 @@
 
 <h1 class="">j</h1>
 <h2 class="">go home</h2>
+
+<img src="./img/1.jpg" alt="" class="">
+
 <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore molestiae dolor a corrupti iure eos natus sit ea aliquam soluta, enim vel tempora nemo architecto odit quas nesciunt, tempore cum!</p>
    
 </body>
