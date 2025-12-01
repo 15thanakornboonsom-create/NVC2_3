28684a98183c696ff0cdbb3c3e7744e8096b7758
fourthrainy-sudo <time.1430ll@gmail.com>
2025-12-01 10:21:57 +0700

    d

--- 114-kongkiat/index.php
+++ 114-kongkiat/index.php
@@ -1,41 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
+
+
 <head>
-
-
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>18+</title>
+    
+    <!-- ชื้อเว็บไซต์ -->
+    <title>Meat</title>
 </head>
-<link rel="stylesheet" href="style.css">
+
+
 
 <body>
 
-<div class="div">
-    <h1 class="h1">ez ez ez</h1>
-</div>
+<!-- ส่วนของNavbar -->
 
+<!-- ส่วนของเนื้อหา -->
 
-<h2 class="h2">home</h2>
+<!-- ส่วนของ footer -->
 
-<img src="./img/1.jpg" alt="" class="">
-
-<p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae similique fugit ea deserunt ab nihil, quos iusto veniam a culpa iure sed! Est fuga placeat ut? Nostrum ut velit sit!
-Ab soluta similique consectetur reprehenderit ipsum eaque illum odio tenetur, eligendi libero asperiores magnam voluptatibus ex officiis voluptas. Molestias assumenda illum consectetur necessitatibus quam eos, magni fugiat obcaecati aspernatur earum?
-Quasi laudantium dolore consequuntur vero nihil necessitatibus, exercitationem, quae molestias incidunt corrupti ea officia recusandae vel ab assumenda laboriosam nam asperiores commodi sint quibusdam! Tempore consequatur animi vero adipisci accusantium.
-Tempore vero accusantium quo consequuntur, veritatis iste minima voluptate cum, ipsum laborum inventore commodi aspernatur officia quaerat, labore neque consequatur nihil voluptates. Expedita et mollitia ullam earum repudiandae dolores quia.
-Unde nobis libero, ratione neque sit impedit illum accusamus eius facilis nemo minus odio placeat reprehenderit id hic temporibus perferendis quo molestias ducimus, deleniti quos. Maiores ab qui illo sequi!
-Architecto, laboriosam eaque saepe sequi molestias quasi ipsam, autem, beatae a repellendus nulla aliquid. Nostrum id quasi aut hic aliquam fugit, libero minus assumenda cupiditate sequi? Vero voluptas ad veniam.
-Rerum temporibus commodi soluta ratione. Autem rerum ea consequatur hic voluptates ipsa, minus obcaecati labore optio nisi impedit odit iure praesentium pariatur perspiciatis voluptate quidem illo sequi? Officiis, enim repellendus.
-Harum accusantium, ducimus cumque veritatis corporis minus voluptatem magni quos atque eum. Recusandae ratione, odit maxime, quis sequi voluptate consequatur voluptatibus debitis tempore praesentium molestias omnis ex dicta eius quia?
-Doloremque atque illum similique, debitis doloribus nisi aliquam unde voluptates deleniti minus incidunt obcaecati laborum ipsam eos adipisci quas dolore minima labore? Temporibus tenetur autem nobis molestiae necessitatibus nam accusamus.
-Vero adipisci maxime modi, est voluptate omnis, quos officiis provident beatae aut ipsum rem, aperiam eos quisquam molestiae? Possimus fugit quas aspernatur laudantium ipsa quibusdam accusantium. Illum saepe eligendi velit!
-Fugiat voluptatum voluptatibus tempora, nobis alias, autem tenetur eaque quibusdam adipisci similique a aut dignissimos obcaecati velit cupiditate consequuntur. Nostrum tempora, quibusdam ipsum cumque unde ad ipsam praesentium maxime obcaecati.
-Ratione ad dolorem itaque cumque labore aliquid, excepturi maiores officiis eius delectus tempora id iure autem repellendus odio suscipit illo quae nemo veritatis! Sint blanditiis quia illum libero aspernatur beatae.
-Earum cum eaque, laudantium reprehenderit maxime vel, accusamus quibusdam esse perferendis, dolorum veritatis vero. Non aperiam veniam praesentium quae rem deleniti ea dolore. Quos ad tempore voluptates laudantium quaerat et!
-Consequuntur cupiditate repellat culpa ipsum ex vitae. Consequatur at temporibus quaerat atque reiciendis debitis vero asperiores fugit? Quos sapiente ipsum delectus at, a veniam labore natus accusamus, error corporis exercitationem?
-Nostrum est eius deserunt dolorum adipisci cupiditate nemo distinctio eaque, amet ratione hic in temporibus exercitationem saepe quidem, quia nihil commodi veritatis molestias ducimus quam officiis nisi. Quaerat, nisi recusandae?</p>
+</body>
 
 
 
